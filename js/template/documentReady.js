7d8cde07119d5a7526aed65c14e3fd9597bab56f
Nicole <nicole.lopez@dice205.com>
2019-10-24 16:38:37 +0800

    java script script ready

---
+++ js/template/documentReady.js
@@ -0,0 +1,12 @@
+const callback = function () {
+    console.log("Script ready");
+}
+
+if (
+    document.readyState === "complete" ||
+    (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+    callback();
+} else {
+    document.addEventListener("DOMContentLoaded", callback);
+}
